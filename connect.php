e8cc59bb6f4527447e8a3c8d4b39b4740f4306d2
valeriooooooo <valeriomalagoli08@gmail.com>
2025-11-25 11:27:34 +0100

    bezig met de admin pagina

---
+++ connect.php
@@ -0,0 +1,15 @@
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "ua_db";
+    // maak connectie
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connectie
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Connected bericht
+    $conn->set_charset("utf8"); // Set the character set to UTF-8
+    ?>
